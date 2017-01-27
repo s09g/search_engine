5bbc963d4015776c9da4341f857d898b7cced79d
zhangshiqiu <zhangshiqiu1993@126.com>
2017-01-26 16:49:04 -0800

    add language model reducer

--- ngram/src/main/java/LanguageModel.java
+++ ngram/src/main/java/LanguageModel.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.*;
 
 /**
  * Created by zhangshiqiu on 2017/1/26.
@@ -55,9 +56,39 @@ public class LanguageModel {
     }
 
     public static class LanguageModelReducer extends Reducer<Text, Text, DBOutputWritable, NullWritable>{
+        int topK;
+
+        @Override
+        public void setup(Context context){
+            Configuration configuration = context.getConfiguration();
+            topK = configuration.getInt("topK", 5);
+        }
+
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+            TreeMap<Integer, List<String>> treeMap = new TreeMap<>(Collections.<Integer>reverseOrder());
 
+            for (Text text : values){
+                String[] texts = text.toString().trim().split("=");
+                String word = texts[0].trim();
+                int count = Integer.parseInt(texts[1].trim());
+
+                if (!treeMap.containsKey(count)){
+                    treeMap.put(count, new ArrayList<String>());
+                }
+                treeMap.get(count).add(word);
+            }
+
+            Iterator<Integer> iterator = treeMap.keySet().iterator();
+            for (int j = 0; iterator.hasNext() && j < topK; ){
+                int keyCount = iterator.next();
+                List<String> words = treeMap.get(keyCount);
+                for (String following_word: words){
+                    context.write(new DBOutputWritable(key.toString(), following_word, keyCount),
+                                                        NullWritable.get());
+                    j++;
+                }
+            }
         }
     }
 }
