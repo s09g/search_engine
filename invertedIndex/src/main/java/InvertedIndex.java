a71a6333894fb2352abb751e82f3920b53728fec
zhangshiqiu <zhangshiqiu1993@126.com>
2017-03-05 23:09:33 -0800

    driver class

--- invertedIndex/src/main/java/InvertedIndex.java
+++ invertedIndex/src/main/java/InvertedIndex.java
@@ -1,46 +1,14 @@
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 /**
  * Created by zhangshiqiu on 2017/2/7.
  */
 public class InvertedIndex {
-    public static class DocumentMapper extends Mapper<Object, Document, Text, IntWritable> {
-        @Override
-        public void map (Object key, Document value, Context context) throws IOException, InterruptedException{
-            int id = value.id;
-            String[] words = value.content.trim().split("\\s+");
-            for (String word : words){
-                context.write(new Text(word), new IntWritable(id));
-            }
-        }
-    }
-
-    public static class IndexReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-        @Override
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
-            Set<Integer> set = new HashSet<Integer>();
-            for (IntWritable value : values) {
-                int val = value.get();
-                if (set.contains(value.get())){
-                    continue;
-                }
-                set.add(val);
-                context.write(key, value);
-            }
-        }
-    }
-
     public static void main(String[] args) throws Exception {
         Configuration configuration = new Configuration();
         Job job = Job.getInstance(configuration);
