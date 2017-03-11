f657af264c6f6ac5b1c2574c1068923cd49e441a
zhangshiqiu <zhangshiqiu1993@126.com>
2017-03-11 12:56:30 -0800

    use two map reduce job to complete this task

--- invertedIndex/src/main/java/InvertedIndex.java
+++ invertedIndex/src/main/java/InvertedIndex.java
@@ -3,12 +3,48 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 /**
- * Created by zhangshiqiu on 2017/3/5.
+ * Created by zhangshiqiu on 2017/3/11.
  */
 public class InvertedIndex {
+    public static class DocumentMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private Text word = new Text();
+        private IntWritable one = new IntWritable();
+        @Override
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String[] id_content = value.toString().trim().split("\t");
+            String documentID = id_content[0];
+            StringTokenizer tokenizer = new StringTokenizer(id_content[1]);
+
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken() + " " + documentID);
+                context.write(word, one);
+            }
+        }
+    }
+    public static class IndexReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+            int sum = 0;
+
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Configuration configuration = new Configuration();
         Job job = Job.getInstance(configuration);
